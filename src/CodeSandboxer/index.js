190b8db3bc620574ca6af80557092ff46abe745a
Ben Conolly <ben@thinkmill.com.au>
2018-04-09 16:14:47 +1000

    Fixing casing part 2

--- src/CodeSandboxer/index.js
+++
@@ -1,9 +0,0 @@
-// @flow
-export { default as parseFile } from './parseFile';
-export { default as replaceImports } from './replaceImports';
-export { default as fetchRelativeFile } from './fetchRelativeFile';
-export { default as fetchFiles } from './fetchFiles';
-export { default as sendFilesToCSB } from './sendFilesToCSB';
-export { getSandboxUrl } from './constants';
-
-export * from './types';
