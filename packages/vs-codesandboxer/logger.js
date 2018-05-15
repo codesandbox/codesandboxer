59f88324a525d8486aa816f9e350eac4bd052463
Ben Conolly <ben@thinkmill.com.au>
2018-05-15 10:17:26 +1000

    Initial working vs-codesandboxer implementation

---
+++ packages/vs-codesandboxer/logger.js
@@ -0,0 +1,10 @@
+const vscode = require('vscode');
+
+const outputChannel = vscode.window.createOutputChannel('codesandboxer')
+
+function log(msg) {
+    console.log(msg)
+    outputChannel.appendLine(msg)
+}
+
+module.exports = log;
