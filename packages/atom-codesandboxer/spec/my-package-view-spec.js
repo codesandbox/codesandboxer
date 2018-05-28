a1cee9e2a992e244535a02dac9eb7f27875a4507
Ben Conolly <ben@thinkmill.com.au>
2018-05-28 12:29:35 +1000

    Move atom-codesandboxer out to its own repo.
    apm appears to be checking whether the package root is a git repository
    so it cannot live in this monorepo

--- packages/atom-codesandboxer/spec/my-package-view-spec.js
+++
@@ -1,9 +0,0 @@
-'use babel';
-
-import MyPackageView from '../lib/my-package-view';
-
-describe('MyPackageView', () => {
-  it('has one valid test', () => {
-    expect('life').toBe('easy');
-  });
-});
