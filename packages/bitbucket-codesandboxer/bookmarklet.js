0cb417dd70f9c7f46e4ad84cd88e3a69068ce28a
lukebatchelor <batchelor.luke+github@gmail.com>
2018-06-01 12:22:33 +1000

    Adds bitbucket-codesandboxer addon (#19)
    * Initial commit of bitbucket-codesandboxer package
    
    * Makes importReplacement array optional in codesandboxer
    
    * Adds full apps for bb pullrequests and fileviews, support for gh links
    
    * Adds bookmarklet code for BB and GH
    
    * Removes unused dep
    
    * Removes console.logs
    
    * Removes oauth token
    
    * Adds unchecked in oauth token
    
    * Fixes flow in react-codesandboxer

---
+++ packages/bitbucket-codesandboxer/bookmarklet.js
@@ -0,0 +1,33 @@
+/**
+ * This bookmarklet can be run from any url that has a file open in BB or GH. Branches, commits, PR's, etc will all work.
+ * Simply save it as a bookmark with "javascript: <paste>" and you should be good to go
+ */
+
+(() => {
+  let parseUrl = (url) => {
+    let matcher;
+    if (url.match(/^https?:\/\/bitbucket.org/)) {
+      matcher = /^https?:\/\/(bitbucket).org\/(.+?)\/(.+?)\/src\/(.+?)\/([^?#]+)/;
+    } else if (!!url.match(/^https?:\/\/github.com/)) {
+      matcher = /^https?:\/\/(github).com\/(.+?)\/(.+?)\/blob\/(.+?)\/([^?#]+)/;
+    } else {
+      return;
+    }
+    const [, host, repoOwner, repoSlug, commit, file] = url.match(matcher);
+    return {
+      host,
+      repoOwner,
+      repoSlug,
+      commit,
+      file
+    };
+  };
+  let location = window.location.href;
+  let parsed = parseUrl(location);
+  if (!parsed) {
+    alert('Something went wrong! We couldnt parse your url!');
+  }
+  let encodedFile = encodeURIComponent(parsed.file);
+  let url = `https://bitbucket-codesandboxer.netlify.com/?host=${parsed.host}&repoOwner=${parsed.repoOwner}&repoSlug=${parsed.repoSlug}&commit=${parsed.commit}&file=${encodedFile}`;
+  window.open(url, '_blank', '');
+})();
---
+++ packages/bitbucket-codesandboxer/src/components/GitFileExplorer.js
@@ -0,0 +1,104 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import pMemoize from 'p-memoize';
+import AsyncSelect from 'react-select/lib/Async';
+import * as bitbucket from '../utils/bitbucket';
+
+type State = {
+  inputValue: string
+};
+const propTypes = {
+  onFileSelect: PropTypes.func,
+  repoName: PropTypes.string,
+  repoOwner: PropTypes.string,
+  repoRef: PropTypes.string,
+};
+
+
+const memoizedBBCall = pMemoize(bitbucket.getBitbucketDir);
+
+function bbDirListToOptions(dirList) {
+  return dirList.map(file => {
+    const isDir = file.type === 'directory';
+    const label = isDir ? `📂 ${file.path}/` : `📄 ${file.path}`;
+    const value = isDir ? `${file.path}/` : file.path;
+    return {
+      value,
+      label,
+      isDir
+    };
+  });
+}
+
+function inputValueToDirPath(inputValue) {
+  const re = /(\S+\/).*/;
+  const match = inputValue.match(re);
+  if (!match) return '/';
+  return match[1];
+}
+
+function filterOptions(options, inputValue) {
+  if (!inputValue) return options;
+  return options.filter(option => option.value.startsWith(inputValue));
+}
+
+export default class GitFileExplorer extends Component<*, State> {
+  state = {
+    inputValue: '',
+    menuIsOpen: true,
+    message: 'Uploading packages/core/avatar/examples/BasicAvatar.jsx'
+  };
+  handleInputChange = (newValue: string, actionMeta) => {
+    const { action } = actionMeta;
+    if (action === 'input-change' || actionMeta.custom) {
+      this.setState({ inputValue: newValue || '' });
+      return newValue;
+    }
+    return this.state.inputValue;
+  };
+  handleSetValue = selected => {
+    this.setState({
+      inputValue: selected.value,
+      menuIsOpen: selected.isDir,
+      message: !selected.isDir ? 'Uploading ' + selected.value : ''
+    });
+    // THIS IS NAUGHTY
+    this.selectRef.handleInputChange(selected.value, { custom: true });
+    if (!selected.isDir) {
+      this.props.onFileSelect(selected.value);
+    }
+  };
+  getOptions = async (inputValue) => {
+    const { repoOwner, repoName, branch } = this.props;
+    const gitInfo = {
+      account: repoOwner,
+      repository: repoName,
+      branch
+    };
+    const dirPath = inputValueToDirPath(inputValue);
+    const dir = await memoizedBBCall(gitInfo, dirPath);
+    const options = bbDirListToOptions(dir);
+    return filterOptions(options, inputValue);
+  }
+  render() {
+    return (
+      <div>
+        <AsyncSelect
+          loadOptions={this.getOptions}
+          defaultOptions
+          onInputChange={this.handleInputChange}
+          closeMenuOnSelect={false}
+          onChange={this.handleSetValue}
+          menuIsOpen={this.state.menuIsOpen}
+          inputValue={this.state.inputValue}
+          ref={ref => {
+            this.selectRef = ref;
+          }}
+        />
+        <div style={{ marginTop: '10px' }}>{this.state.message}</div>
+      </div>
+    );
+  }
+}
+GitFileExplorer.propTypes = propTypes;
+
