4de9d566afe5bc16f4e97ed1b34d8d59e23967a1
Ben Conolly <ben@thinkmill.com.au>
2018-06-01 11:34:16 +1000

    separate build commands for individual packages

--- packages/codesandboxer-fs/index.js
+++ packages/codesandboxer-fs/index.js
@@ -1,13 +1,11 @@
 // @flow
-const assembleFiles = require('./assembleFiles');
-const assembleFilesAndPost = require('./assembleFilesAndPost');
+const assembleFiles = require('./src/assembleFiles');
+const assembleFilesAndPost = require('./src/assembleFilesAndPost');
 
 /* eslint-disable-next-line */
 module.exports = { assembleFilesAndPost, assembleFiles };
 
 // other stuff we need to handle:
 // loading nonJS files
-//   .jsx
-//   .png +
 //   .tsx
 // Check we never reach above our initial scope
