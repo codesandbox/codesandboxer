0527dd38e4182f2893b735b30354c4484ceb9377
Ben Conolly <ben@thinkmill.com.au>
2018-11-15 14:51:10 +1100

    Noviny/add gifs (#35)
    * Adding gifs of how things work to main documentation
    
    * fix typescript importing react

---
+++ fixtures/look-what-we-can-do/hidden-bonus.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default () => (
+  <div>
+    <p>
+      An important feature of codesandboxer is it allows you to include other
+      files, and you don't have to worry about including them. Things like this
+      file.
+    </p>
+    <p>
+      This means as long as you have an example file to begin with, it should
+      Just Work™.
+    </p>
+  </div>
+);
